4e2b03e44639076980760ace1574bc72b5a401c7
Cyfurion <patrickfan620@gmail.com>
2019-03-11 14:49:14 -0400

    Create abstract Organ.java

--- src/Organ.java
+++ src/Organ.java
@@ -1,3 +1,20 @@
 public abstract class Organ {
+    private int priority, capacity;
+    private double currentHealth, totalHealth, percentageControl, resistance;
+    private boolean playerControl;
 
+    public Organ(double health, int capacity, double resistance, int priority) {
+        this.priority = priority;
+        this.currentHealth = health;
+        this.totalHealth = health;
+        this.capacity = capacity;
+        this.resistance = resistance;
+        playerControl = true;
+        percentageControl = 1;
+    }
+
+    public boolean getPlayerControl() { return playerControl; }
+    public int getCapacity() { return capacity; }
+
+    public abstract void applyPenalties();
 }
