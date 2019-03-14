815a3687f51f5b7ba5d5b1862fa2b0d505be0a36
Cyfurion <patrickfan620@gmail.com>
2019-03-14 14:35:57 -0400

    Create Heart.java

---
+++ src/Heart.java
@@ -0,0 +1,15 @@
+public class Heart extends Organ {
+
+    public Heart() {
+        super(35000, 750, 0.5, 0, "Heart");
+    }
+
+    public void applyCollapsePenalties(ImmuneSystem system) {
+
+    }
+
+    public void applyDamage(Pathogen cell) {
+
+    }
+
+}
--- src/Organ.java
+++ src/Organ.java
@@ -1,20 +1,45 @@
 public abstract class Organ {
-    private int priority, capacity;
-    private double currentHealth, totalHealth, percentageControl, resistance;
-    private boolean playerControl;
+    private int priority, currentCapacity;
+    private final int maxCapacity;
+    private double currentHealth, resistance;
+    private final double maxHealth;
+    private String name;
 
-    public Organ(double health, int capacity, double resistance, int priority) {
+    public Organ(double health, int capacity, double resistance, int priority, String name) {
         this.priority = priority;
         this.currentHealth = health;
-        this.totalHealth = health;
-        this.capacity = capacity;
+        this.maxHealth = health;
+        this.maxCapacity = capacity;
+        currentCapacity = 0;
         this.resistance = resistance;
-        playerControl = true;
-        percentageControl = 1;
+        this.name = name;
     }
 
-    public boolean getPlayerControl() { return playerControl; }
-    public int getCapacity() { return capacity; }
+    public int getMaxCapacity() { return maxCapacity; }
+    public int getCurrentCapacity() { return currentCapacity; }
+    public int getPriority() { return priority; }
+    public double getResistance() { return resistance; }
+    public boolean getPlayerControl() { return currentHealth > 0; }
+    public String getName() { return name; }
 
-    public abstract void applyPenalties();
+    public boolean changeCapacity(int delta) {
+        if (delta < 0 && currentCapacity == 0 || delta > 0 && currentCapacity == maxCapacity) { return false; }
+        currentCapacity += delta;
+        return true;
+    }
+    public void changeHealth(double delta) {
+        currentHealth += delta;
+        if (currentHealth < 0) {
+            currentHealth = 0;
+        } else if (currentHealth > maxHealth) {
+            currentHealth = maxHealth;
+        }
+    }
+    public void changeResistance(double delta) {
+        resistance += delta;
+        if (resistance > 1) { resistance = 1; }
+    }
+
+    public abstract void applyCollapsePenalties(ImmuneSystem system);
+    public abstract void applyDamage(Pathogen cell);
 }
