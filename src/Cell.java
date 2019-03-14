83d14f9f69cf818e3f140f83be955ca8c85fe498
Ceiran <anaito8367@bths.edu>
2019-03-14 14:11:33 -0400

    Finished Cell class.

--- src/Cell.java
+++ src/Cell.java
@@ -1,5 +1,20 @@
 public class Cell {
     private int health, damage, attackSpeed, infectionShield;
     private double accuracy;
+    private String type;
 
+    public Cell(int health, int damage, int attackSpeed, int infectionShield, double accuracy, String type){
+        this.health = health;
+        this.damage = damage;
+        this.attackSpeed = attackSpeed;
+        this.infectionShield = infectionShield;
+        this.accuracy = accuracy;
+        this.type = type;
+    }
+
+    public int getHealth(){ return health; }
+    public int getDamage(){ return damage; }
+    public int getAttackSpeed(){ return attackSpeed; }
+    public int getInfectionShield(){ return infectionShield; }
+    public double getAccuracy(){ return accuracy; }
 }
