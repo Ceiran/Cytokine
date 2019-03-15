531b4cad08a706f4655797dab1ecf0a7b441ae3d
Ceiran <anaito8367@bths.edu>
2019-03-15 14:01:22 -0400

    Implement additional methods and instance variables for Cell class

--- src/Cell.java
+++ src/Cell.java
@@ -1,10 +1,11 @@
 public abstract class Cell {
-    private int health, damage, attackSpeed, infectionShield;
+    private int health, maxHealth, damage, attackSpeed, infectionShield;
     private double accuracy;
     private String type;
 
-    public Cell(int health, int damage, int attackSpeed, int infectionShield, double accuracy, String type) {
+    public Cell(int health, int maxHealth, int damage, int attackSpeed, int infectionShield, double accuracy, String type) {
         this.health = health;
+        this.maxHealth = maxHealth;
         this.damage = damage;
         this.attackSpeed = attackSpeed;
         this.infectionShield = infectionShield;
@@ -13,13 +14,28 @@ public abstract class Cell {
     }
 
     public int getHealth() { return health; }
+    public int getMaxHealth() { return maxHealth; }
     public int getDamage() { return damage; }
     public int getAttackSpeed() { return attackSpeed; }
     public int getInfectionShield() { return infectionShield; }
     public double getAccuracy() { return accuracy; }
-    public int getPercentage() { return () }
+    public String getType() { return type; }
 
-    public void changeHealth(int delta){
-        health += delta;
+    public double getPercentage() { return Math.round(((double)health / maxHealth) * 100.0) / 100.0; }
+
+    /* Calculates total health of a cell after a change, delta, is applied.
+       Flat is true if delta is a flat rate and false if it is a percent change.
+     */
+    public void changeHealth(double delta, boolean flat) {
+        if (flat) {
+            if ((health += delta) < 0) {
+                health = 0;
+            } else if ((health += delta) > maxHealth) {
+                health = maxHealth;
+            } else
+                health += delta;
+        } else {
+            health *= (1 + delta);
+        }
     }
 }
