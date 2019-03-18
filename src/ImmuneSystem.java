406688f0450ecec0ba4177446e44da53ee32fcd0
BT_1N3_21 <wtsang1931@bths.edu>
2019-03-18 11:10:37 -0400

    Implements unfinished ImmuneSystem classes

--- src/ImmuneSystem.java
+++ src/ImmuneSystem.java
@@ -1,8 +1,9 @@
 public class ImmuneSystem {
-    private int cell, money;
+    private int  money;
+    private boolean cell;
     private String cytokine, regen, boost, freeze, fever;
 
-    public ImmuneSystem(int cell, int money, String regen, String boost, String freeze, String fever){
+    public ImmuneSystem(boolean cell, int money, String regen, String boost, String freeze, String fever){
         this.cell = cell;
         this.money = money;
         this.regen = regen;
@@ -11,12 +12,16 @@ public class ImmuneSystem {
         this.fever = fever;
     }
 
-    public int getCells(){return cell;}
+    public boolean getCells(){return cell;}
     public int getMoney(){return money;}
     public String getRegen(){return regen; }
     public String getBoost(){return boost;}
     public String getFreeze(){return freeze;}
     public String getFever(){return fever;}
 
-    
+    public void makeCells(){
+        if(getCells()){
+
+        }
+    }
 }
