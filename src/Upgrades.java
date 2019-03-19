dd30d9edac3a27708a76bccb535a302b82dc2b97
BT_1N3_21 <wtsang1931@bths.edu>
2019-03-19 11:41:05 -0400

    implement unfinished upgrades

--- src/Upgrades.java
+++ src/Upgrades.java
@@ -1,4 +1,14 @@
 public abstract class Upgrades {
+    private int health, damage, infectionShield, Accuracy, ATPConsumption;
+    private double attackSpeed;
 
-    
+    public Upgrades(int health, int damage, int infectionShield, int Accuracy, int ATPConsumption, double attackSpeed){
+        this.health = health;
+        this.damage = damage;
+        this.infectionShield = infectionShield;
+        this.Accuracy = Accuracy;
+        this.ATPConsumption = ATPConsumption;
+        this. attackSpeed = attackSpeed;
+    }
+
 }
