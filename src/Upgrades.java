7d02498680208c5abbac6c0fde8815c91e07ead6
BT_1N3_21 <wtsang1931@bths.edu>
2019-03-26 11:20:36 -0400

    Added to upgrades

--- src/Upgrades.java
+++ src/Upgrades.java
@@ -1,14 +1,20 @@
 public abstract class Upgrades {
-    private int health, damage, infectionShield, Accuracy, ATPConsumption;
+    private int health, damage, infectionShield, accuracy, ATPConsumption;
     private double attackSpeed;
 
-    public Upgrades(int health, int damage, int infectionShield, int Accuracy, int ATPConsumption, double attackSpeed){
+    public Upgrades(int health, int damage, int infectionShield, int accuracy, int ATPConsumption, double attackSpeed){
         this.health = health;
         this.damage = damage;
         this.infectionShield = infectionShield;
-        this.Accuracy = Accuracy;
+        this.accuracy = accuracy;
         this.ATPConsumption = ATPConsumption;
-        this. attackSpeed = attackSpeed;
+        this.attackSpeed = attackSpeed;
     }
+    public int getHealth() { return health; }
+    public int getDamage() { return damage; }
+    public int getInfectionShield() { return infectionShield; }
+    public int getaccuracy() { return accuracy; }
+    public int getATPConsumption() { return ATPConsumption; }
+    public double getAttackSpeed() { return attackSpeed; }
 
 }
