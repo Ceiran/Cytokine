23b87f6eb904b0f8be5ff9b9d7274b466518ec5d
Ceiran <anaito8367@bths.edu>
2019-04-11 14:42:30 -0400

    Fix BCell health

--- src/ATPSynthase.java
+++ src/ATPSynthase.java
@@ -1,14 +1,16 @@
 public class ATPSynthase extends ImmuneCell {
     private int atpProduction;
+    private int lifeTurns;
 
     public ATPSynthase(int health, int maxHealth, int damage, int recharge, double accuracy, int infectionShield, String name) {
         super(1, 1, 0, 0, 0, 0, "ATPSynthase");
         atpProduction = 2;
+        lifeTurns = 45;
     }
 
     public int getAtpProduction() { return atpProduction; }
 
     public void attack(Cell target) {
-        // ATPSynthase Cells cannot attack.
+        // ATPSynthase cannot attack.
     }
 }
--- src/BCell.java
+++ src/BCell.java
@@ -1,9 +1,11 @@
 public class BCell extends ImmuneCell {
     private int antibodyProduction;
+    private int lifeTurns;
 
     public BCell() {
-        super(0, 0, 0, 0, 0, 0, "BCell");
+        super(50, 50, 0, 0, 0, 0, "BCell");
         antibodyProduction = 12;
+        lifeTurns = 30;
     }
 
     public int getAntibodyProduction() { return antibodyProduction; }
