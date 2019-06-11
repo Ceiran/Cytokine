91451ec00649663fdc5695bda70eb5aef7df5c78
Cyfurion <patrickfan620@gmail.com>
2019-06-11 13:31:01 -0400

    Add default cell count at game start

--- src/ATPSynthase.java
+++ src/ATPSynthase.java
@@ -2,7 +2,7 @@ public class ATPSynthase extends ImmuneCell {
     private int atpProduction;
     private int lifeTurns;
 
-    public ATPSynthase(int health, int maxHealth, int damage, int recharge, double accuracy, int infectionShield, String name) {
+    public ATPSynthase() {
         super(1, 1, 0, 0, 0, 0, "ATPSynthase");
         atpProduction = 2;
         lifeTurns = 45;
