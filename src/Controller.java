13b3a85b905bd8532e8729349264927a7c729333
ajcdarkness <33042703+ajcdarkness@users.noreply.github.com>
2019-06-12 00:40:41 -0400

    Attempts to change scenes with controller

--- src/Controller.java
+++ src/Controller.java
@@ -1,32 +1,20 @@
-import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.fxml.FXML;
-import java.io.IOException;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Group;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.fxml.Initializable;
-import javafx.event.ActionEvent;
-
-import java.io.IOException;
-import java.net.URL;
-import java.util.ResourceBundle;
-
-import javafx.scene.control.TableView;
-import javafx.scene.layout.AnchorPane;
-
 public class Controller {
-    @FXML private AnchorPane startMenu;
 
-    @FXML private void submit(ActionEvent event) throws IOException {
 
-        AnchorPane pane = FXMLLoader.load(this.getClass().getResource("startMenu.fxml"));
-        Game.getChildren().setAll(pane);
+
+    @FXML private Label label;
+    @FXML private TextField field;
+    private Execute main;
+
+    public void setMain(Execute main){
+        this.main = main;
+    }
+
+    public void handleButton(){
+        String text = field.getText();
+        label.setText(text);
     }
 }
-
