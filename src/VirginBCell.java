4cdc7c5cf01f9cfe294bcafd3810659804d76790
Cyfurion <patrickfan620@gmail.com>
2019-06-12 13:53:39 -0400

    Update virgin cells

--- src/VirginBCell.java
+++ src/VirginBCell.java
@@ -1,5 +1,4 @@
 public class VirginBCell extends ImmuneCell {
-    private int spawnTime;
 
     public VirginBCell(int spawn) {
         super(Stats.virginBHealth,
@@ -9,12 +8,10 @@ public class VirginBCell extends ImmuneCell {
                 Stats.virginBAccuracy,
                 Stats.virginBInfectionShield,
                 Stats.virginBName);
-        spawnTime = Stats.virginBSpawnTime;
     }
-
-    public int getSpawnTime() { return spawnTime; }
 
     public void attack(Cell target) {
         // VirginBCells cannot attack.
     }
+
 }
--- src/VirginTCell.java
+++ src/VirginTCell.java
@@ -1,5 +1,4 @@
 public class VirginTCell extends ImmuneCell {
-    private int spawnTime;
     
     public VirginTCell(int spawn) {
         super(Stats.virginTHealth,
@@ -9,12 +8,10 @@ public class VirginTCell extends ImmuneCell {
                 Stats.virginTAccuracy,
                 Stats.virginTInfectionShield,
                 Stats.virginTName);
-        spawnTime = Stats.virginTSpawnTime;
     }
-
-    public int getSpawnTime() { return spawnTime; }
 
     public void attack(Cell target) {
         // VirginTCells cannot attack.
     }
+
 }
