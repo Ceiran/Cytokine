1e1162db931c7f5d8f814161a0216cf0e7622bc8
ajcdarkness <33042703+ajcdarkness@users.noreply.github.com>
2019-06-13 02:21:20 -0400

    made fxml run ish

--- src/Controller.java
+++ src/Controller.java
@@ -1,29 +1,12 @@
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.fxml.FXML;
-import javafx.stage.Stage;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 
 public class Controller {
-    Scene Game, startMenu;
+    public void gameStart(ActionEvent event){
+        Execute.executeButBetter();
 
-
-    @FXML private Label label;
-    @FXML private TextField field;
-    private Execute main;
-
-    public void setMain(Execute main){
-        this.main = main;
-    }
-    public void handleButton(Stage primaryStage){
-        primaryStage.setScene(Game);
 
     }
-    public void deploy(){
-
-    }
-    public void healthBar(){
-
-    }
-
 }
--- src/Execute.java
+++ src/Execute.java
@@ -1,30 +1,25 @@
+import javafx.application.Application;
 import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import java.io.IOException;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Group;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.fxml.Initializable;
-import javafx.event.ActionEvent;
+import javafx.scene.layout.StackPane;
+import javafx.scene.Parent;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
-import javafx.application.Application;
-import javafx.scene.control.Label;
-import java.io.IOException;
-import java.net.URL;
-import java.util.ResourceBundle;
-
-import javafx.scene.control.TableView;
-import javafx.scene.layout.AnchorPane;
-
 public class Execute extends Application{
-    Scene Game, startMenu;
+
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("start.fxml"));
+        primaryStage.setTitle("Cytokine");
+        primaryStage.setScene(new Scene(root,800,600));
+        primaryStage.show();
+
+    }
     public static void main(String[] args) {
+        launch(args);
+    }
+    public static void executeButBetter(){
         Game game = new Game(0);
         long initial = System.currentTimeMillis();
         while (game.runTurn()) {
@@ -32,9 +27,6 @@ public class Execute extends Application{
             initial = System.currentTimeMillis();
         }
         System.out.println("TERMINATE GAME"); // End condition reached, patient is dead or pathogen is destroyed.
-    }
-    public void start(Stage primaryStage) throws Exception {
-        Button button1 = new Button("Go to scene two");
-        button1.setOnAction(e -> primaryStage.setScene(Game));
+
     }
 }
