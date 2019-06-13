5fb94718d5bfc0b22ccadc8f4af8428453ac63f1
Ayane Naito <anaito8367@bths.edu>
2019-06-12 23:46:21 -0400

    Fix formatting again

--- src/CommonCold.java
+++ src/CommonCold.java
@@ -2,6 +2,4 @@ public class CommonCold extends Pathogen {
     public CommonCold(){
         super(Stats.commonColdHealth, Stats.commonColdMaxHealth, Stats.commonColdDamage, Stats.commonColdRecharge, Stats.commonColdAccuracy, Stats.commonColdInfectionDamage, Stats.commonColdDuplicationSPD, Stats.commonColdName);
     }
-
-
 }
