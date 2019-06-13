67db328ba61df176052d88b394ec8b78d3715646
Cyfurion <patrickfan620@gmail.com>
2019-06-13 00:56:45 -0400

    Fix merge errors

--- src/VirginBCell.java
+++ src/VirginBCell.java
@@ -2,7 +2,6 @@ public class VirginBCell extends ImmuneCell {
 
     public VirginBCell() {
         super(Stats.virginBHealth, Stats.virginBMaxHealth, Stats.virginBDamage, Stats.virginBRecharge, Stats.virginBOrganHealRate, Stats.virginBAccuracy, Stats.virginBInfectionShield, Stats.virginBName);
-        spawnTime = Stats.virginBSpawnTime;
     }
 
     public void attack(Cell target) {
